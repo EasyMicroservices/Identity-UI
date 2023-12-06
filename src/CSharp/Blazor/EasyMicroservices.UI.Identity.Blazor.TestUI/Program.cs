294bf916a2b24e3792a6e87d59db118d5b40f393
Ali Yousefi <ali.visual.studio@gmail.com>
2023-12-07 00:23:40 +0330

    init

---
+++ src/CSharp/Blazor/EasyMicroservices.UI.Identity.Blazor.TestUI/Program.cs
@@ -0,0 +1,23 @@
+using EasyMicroservices.UI.Identity.Blazor.TestUI;
+using EasyMicroservices.UI.Identity.ViewModels.Authentications;
+using Identity.GeneratedServices;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new AuthenticationClient("http://localhost:2008", sp.GetService<HttpClient>()));
+
+builder.Services.AddScoped<LoginViewModel>();
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+});
+
+await builder.Build().RunAsync();
---
+++ src/CSharp/Cores/EasyMicroservices.UI.Identity.ViewModels/ViewModels/Authentications/LoginViewModel.cs
@@ -0,0 +1,74 @@
+﻿using EasyMicroservices.UI.Cores;
+using EasyMicroservices.UI.Cores.Commands;
+using Identity.GeneratedServices;
+
+namespace EasyMicroservices.UI.Identity.ViewModels.Authentications
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel(AuthenticationClient authenticationClient)
+        {
+            _authenticationClient = authenticationClient;
+            LoginCommand = new TaskRelayCommand(this, Login);
+            Clear();
+        }
+
+        public TaskRelayCommand LoginCommand { get; set; }
+
+        readonly AuthenticationClient _authenticationClient;
+
+        string _UserName;
+        public string UserName
+        {
+            get => _UserName;
+            set
+            {
+                _UserName = value;
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+
+        string _Password;
+        public string Password
+        {
+            get => _Password;
+            set
+            {
+                _Password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public async Task Login()
+        {
+            await ExecuteApi(async () =>
+            {
+                return await _authenticationClient.LoginAsync(new UserSummaryContract()
+                {
+                    UserName = UserName,
+                    Password = Password
+                });
+            }, (LoginWithTokenResponseContract result) =>
+            {
+                return Task.CompletedTask;
+            });
+        }
+
+
+        public override Task OnError(Exception exception)
+        {
+            return base.OnError(exception);
+        }
+
+        public override Task DisplayFetchError(ServiceContracts.ErrorContract errorContract)
+        {
+            return base.DisplayFetchError(errorContract);
+        }
+
+        public void Clear()
+        {
+            UserName = default;
+            Password = default;
+        }
+    }
+}
