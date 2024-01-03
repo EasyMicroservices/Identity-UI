77fb347a3a4912d800b7b036259ced3d3433db4b
Ali Yousefi <ali.visual.studio@gmail.com>
2024-01-02 23:07:52 -0800

    Support for multi language and right to left converter

--- src/CSharp/Maui/EasyMicroservices.UI.Identity.Maui.TestUI/MauiProgram.cs
+++ src/CSharp/Maui/EasyMicroservices.UI.Identity.Maui.TestUI/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using EasyMicroservices.UI.Identity.Helpers;
+﻿using EasyMicroservices.Domain.Contracts.Common;
+using EasyMicroservices.UI.Cores;
+using EasyMicroservices.UI.Identity.Helpers;
 using EasyMicroservices.UI.Identity.ViewModels.Authentications;
 using Identity.GeneratedServices;
 using Microsoft.Extensions.Logging;
@@ -8,6 +10,9 @@ public static class MauiProgram
 {
     public static MauiApp CreateMauiApp()
     {
+        LoadLanguage("en-US");
+        BaseViewModel.CurrentApplicationLanguage = "en-US";
+        BaseViewModel.IsRightToLeft = false;
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
@@ -27,5 +32,34 @@ public static class MauiProgram
         var build = builder.Build();
         ViewModelLocator.ServiceProvider = build.Services;
         return build;
+    }
+
+    static void LoadLanguage(string languageShortName)
+    {
+        BaseViewModel.AppendLanguage("Username_Title", new LanguageContract()
+        {
+            ShortName = languageShortName,
+            Value = "Username:"
+        });
+        BaseViewModel.AppendLanguage("Password_Title", new LanguageContract()
+        {
+            ShortName = languageShortName,
+            Value = "Password:"
+        });
+        BaseViewModel.AppendLanguage("Login", new LanguageContract()
+        {
+            ShortName = languageShortName,
+            Value = "Login"
+        });
+        BaseViewModel.AppendLanguage("Cancel", new LanguageContract()
+        {
+            ShortName = languageShortName,
+            Value = "Cancel"
+        });
+        BaseViewModel.AppendLanguage("Register", new LanguageContract()
+        {
+            ShortName = languageShortName,
+            Value = "Register"
+        });
     }
 }
---
+++ src/CSharp/Maui/EasyMicroservices.UI.Identity.Maui/Helpers/Language.cs
@@ -0,0 +1,31 @@
+﻿using EasyMicroservices.UI.Cores;
+
+namespace EasyMicroservices.UI.Identity.Maui.Helpers;
+public class Language
+{
+    public static readonly BindableProperty KeyProperty = BindableProperty.CreateAttached("Key",
+        typeof(string), typeof(Language),
+        null,
+        BindingMode.TwoWay,
+        propertyChanged: PropertyChanged);
+
+    public static string GetKey(BindableObject view)
+    {
+        return (string)view.GetValue(KeyProperty);
+    }
+
+    public static void SetKey(BindableObject view, string value)
+    {
+        view.SetValue(KeyProperty, value);
+    }
+
+    static ApiBaseViewModel ViewModel { get; set; } = new ApiBaseViewModel();
+    static void PropertyChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var value = ViewModel.GetLanguage((string)newValue);
+        if (bindable is Button button)
+            button.Text = value + button.Text;
+        else if (bindable is Label label)
+            label.Text = value + label.Text;
+    }
+}
